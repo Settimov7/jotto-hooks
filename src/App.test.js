df0f35dcd04d25c213c660579e43272a10794cd3
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-02-04 10:02:16 +0400

    add loading secret word

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import { findByTestAttr } from '../test/test-utils';
 import App from './App';
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
 
 /**
  * Setup function for app component
- * @returns {ShallowWrapper>}
+ * @returns {ReactWrapper>}
  */
 const setup = () => {
-  return shallow(<App />);
+	mockGetSecretWord.mockClear();
+	hookActions.getSecretWord = mockGetSecretWord;
+
+	return mount(<App/>);
 };
 
 test('App renders without error', () => {
-  const wrapper = setup();
-  const component = findByTestAttr(wrapper, 'component-app');
+	const wrapper = setup();
+	const component = findByTestAttr(wrapper, 'component-app');
 
-  expect(component).toHaveLength(1);
+	expect(component).toHaveLength(1);
+});
+
+describe('getSecretWord calls', () => {
+	test('getSecretWord gets called on App mount', () => {
+		setup();
+
+		expect(mockGetSecretWord).toHaveBeenCalled();
+	});
+
+	test('secretWord does not update on App update', () => {
+		const wrapper = setup();
+
+		mockGetSecretWord.mockClear();
+		wrapper.update();
+
+		expect(mockGetSecretWord).not.toHaveBeenCalled();
+	});
 });
